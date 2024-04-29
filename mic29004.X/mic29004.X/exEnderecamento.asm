8c323d45698c845e06ac3482a2ae5a62559733d9
gg <gian.s12@aluno.ifsc.edu.br>
2024-04-29 20:39:29 -0300

    feito exEndereçamento

--- mic29004.X/mic29004.X/exEnderecamento.asm
+++ mic29004.X/mic29004.X/exEnderecamento.asm
@@ -1,6 +1,65 @@
+.DSEG
+    .ORG SRAM_START
+    A1: .BYTE 12
+    A2: .BYTE 12
+    A3: .BYTE 12
+    A4: .BYTE 3
+    
+.CSEG
 
-
-; Replace with your application code
 start:
+    LDI XH, HIGH(A2)
+    LDI XL, LOW(A2)
+    LDI YH, HIGH(A3)
+    LDI YL, LOW(A3)
+    
+    ldi r16, 0
+LOOP:		; colocando os valores de A2 e A3
     inc r16
+    st X+,r16
+    st Y+,r16
+    cpi r16,12
+    brne LOOP
+    
+    
+    LDI ZH, HIGH(A1)
+    LDI ZL, LOW(A1)
+    
+    ld r0,X
+    ldd r1,Y+11
+    add r0,r1
+    st Z,r0
+    ldi r16,0
+SOMA:		; fazendo soma dos valores
+    ld r0,X+
+    ld r1,-Y
+    add r0,r1
+    st Z+,r0
+    inc r16  
+    cpi r16,12    
+    brlt SOMA
+  
+	
+    LDI XH,HIGH(A4)
+    LDI XL,LOW(A4)
+    LDI ZH,HIGH(A2)
+    LDI ZH,LOW(A2)
+		; fazendo a outra soma
+    ldd r0,Z+2
+    ldd r1,Y+5
+    add r0,r1
+    st X+,r0
+    
+    ldd r0,Z+3
+    ldd r1,Y+4
+    add r0,r1
+    st X+,r0
+    
+    ldd r0,Z+8
+    ldd r1,Y+9
+    add r0,r1
+    st X+,r0
+    
+    
+    
     rjmp start
--- mic29004.X/mic29004.X/nbproject/Makefile-local-default.mk
+++ mic29004.X/mic29004.X/nbproject/Makefile-local-default.mk
@@ -14,11 +14,11 @@
 # You can invoke make with the values of the macros:
 # $ makeMP_CC="/opt/microchip/mplabc30/v3.30c/bin/pic30-gcc" ...  
 #
-PATH_TO_IDE_BIN=/opt/microchip/mplabx/v6.15/mplab_platform/platform/../mplab_ide/modules/../../bin/
+PATH_TO_IDE_BIN=/opt/microchip/mplabx/v6.10/mplab_platform/platform/../mplab_ide/modules/../../bin/
 # Adding MPLAB X bin directory to path.
-PATH:=/opt/microchip/mplabx/v6.15/mplab_platform/platform/../mplab_ide/modules/../../bin/:$(PATH)
+PATH:=/opt/microchip/mplabx/v6.10/mplab_platform/platform/../mplab_ide/modules/../../bin/:$(PATH)
 # Path to java used to run MPLAB X when this makefile was created
-MP_JAVA_PATH="/opt/microchip/mplabx/v6.15/sys/java/zulu8.64.0.19-ca-fx-jre8.0.345-linux_x64/bin/"
+MP_JAVA_PATH="/opt/microchip/mplabx/v6.10/sys/java/zulu8.64.0.19-ca-fx-jre8.0.345-linux_x64/bin/"
 OS_CURRENT="$(shell uname -s)"
 # MP_CC is not defined
 # MP_CPPC is not defined
@@ -26,11 +26,11 @@ OS_CURRENT="$(shell uname -s)"
 MP_AS="/opt/microchip/xc8/v2.05/avr/bin/avrasm2"
 # MP_LD is not defined
 # MP_AR is not defined
-DEP_GEN=${MP_JAVA_PATH}java -jar "/opt/microchip/mplabx/v6.15/mplab_platform/platform/../mplab_ide/modules/../../bin/extractobjectdependencies.jar"
+DEP_GEN=${MP_JAVA_PATH}java -jar "/opt/microchip/mplabx/v6.10/mplab_platform/platform/../mplab_ide/modules/../../bin/extractobjectdependencies.jar"
 # MP_CC_DIR is not defined
 # MP_CPPC_DIR is not defined
 # MP_BC_DIR is not defined
 MP_AS_DIR="/opt/microchip/xc8/v2.05/avr/bin"
 # MP_LD_DIR is not defined
 # MP_AR_DIR is not defined
-DFP_DIR=/opt/microchip/mplabx/v6.15/packs/Microchip/ATmega_DFP/3.0.158
+DFP_DIR=/opt/microchip/mplabx/v6.10/packs/Microchip/ATmega_DFP/3.0.158
